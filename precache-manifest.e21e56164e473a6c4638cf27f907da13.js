f42eb0769820d7cd3f88c9fbb57a4de19a355d88
ronjoo <ronald.joo@coenterprise.com>
2020-02-17 14:26:44 -0500

    Updates

---
+++ precache-manifest.e21e56164e473a6c4638cf27f907da13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73defc6624dcbb41928fb64f68b3c4ba",
+    "url": "/Untitled-Test/index.html"
+  },
+  {
+    "revision": "62ce43977a9af35de12a",
+    "url": "/Untitled-Test/static/css/main.f603deed.chunk.css"
+  },
+  {
+    "revision": "848787316ba877a2628b",
+    "url": "/Untitled-Test/static/js/2.90476adf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Untitled-Test/static/js/2.90476adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ce43977a9af35de12a",
+    "url": "/Untitled-Test/static/js/main.6ef92454.chunk.js"
+  },
+  {
+    "revision": "9c09287d76ad2c27fe16",
+    "url": "/Untitled-Test/static/js/runtime-main.d403136a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Untitled-Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
